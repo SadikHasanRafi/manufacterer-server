4e33e2fe94aed68092a583369ac9630c86a29330
SadikHasanRafi <sadikkhasan@gmail.com>
2022-05-25 20:37:52 +0600

    footer ui done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./Shared/Footer/Footer";
 import Header from "./Shared/Header/Header";
 
 function App() {
   return (
     <div className=" px-8">
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
