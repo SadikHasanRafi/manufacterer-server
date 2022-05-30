619a6b6d8cfbc3719f23bda06669dccf3d06a958
SadikHasanRafi <sadikkhasan@gmail.com>
2022-05-30 21:45:48 +0600

    banner turn off env configure

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -14,17 +14,16 @@ const Banner = () => {
 },[])
 
 
-  const product = products[products.length-1].pic
-  const {pic,name,description} = product
+  console.log(products[products.length-1].pic)
 
 
     return (
       <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row">
-        <img src={pic} className="max-w-sm rounded-lg shadow-2xl" />
+        <img src={products[products.length-1]?.pic} className="max-w-sm rounded-lg shadow-2xl" />
         <div>
-          <h1 className="text-5xl font-bold">{name}</h1>
-          <p className="py-6">{description}</p>
+          <h1 className="text-5xl font-bold">{products[products.length-1]?.name}</h1>
+          <p className="py-6">{products[products.length-1]?.description}</p>
           <Link to='/product'><button className="btn btn-primary">Buy Now</button></Link>
         </div>
       </div>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,20 +1,42 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC4vRkHytjb9ALbzuuZqCJXyED_kQdB9Rg",
-  authDomain: "laptop-accessories.firebaseapp.com",
-  projectId: "laptop-accessories",
-  storageBucket: "laptop-accessories.appspot.com",
-  messagingSenderId: "609999137237",
-  appId: "1:609999137237:web:af05622792d1868715b55c"
+  // apiKey: "AIzaSyC4vRkHytjb9ALbzuuZqCJXyED_kQdB9Rg",
+  // authDomain: "laptop-accessories.firebaseapp.com",
+  // projectId: "laptop-accessories",
+  // storageBucket: "laptop-accessories.appspot.com",
+  // messagingSenderId: "609999137237",
+  // appId: "1:609999137237:web:af05622792d1868715b55c"
+  
+
+
+
+  apiKey : process.env.REACT_APP_apiKey,
+  authDomain : process.env.REACT_APP_authDomain,
+  projectId : process.env.REACT_APP_projectId,
+  storageBucket : process.env.REACT_APP_storageBucket,
+  messagingSenderId : process.env.REACT_APP_messagingSenderId,
+  appId : process.env.REACT_APP_appId
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
